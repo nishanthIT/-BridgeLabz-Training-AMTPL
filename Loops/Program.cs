2e66eaf595daf3c1df8b62eb761a042aadc439cf
N1sh <nishanth23082004@gmail.com>
2025-10-16 16:06:30 +0530

    worked with loops

---
+++ Loops/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("For loop:");
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.WriteLine("\n while looop");
+        int j = 0;
+
+        while (j < 5)
+        {
+            Console.WriteLine(j);
+            j++;
+        }
+
+        Console.WriteLine("\n do while loop");
+        int k = 0;
+        do
+        {
+            Console.WriteLine(k);
+            k++;
+
+        }
+        while (k < 5);
+
+        int[] numbers = { 1, 2, 3, 4, 5 };
+
+        Console.WriteLine("\n foreach loop");
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
+
+
+    }
+}
