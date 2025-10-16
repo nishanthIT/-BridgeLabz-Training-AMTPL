ea5a76b5463fe26232ebd2b497cc15e401d17b76
N1sh <nishanth23082004@gmail.com>
2025-10-16 12:31:47 +0530

    simple if esle

--- BridgeLabsTraining/Program.cs
+++ BridgeLabsTraining/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int a = 10;
+int b = 20;
+
+if(a>b)
+{
+    Console.WriteLine("a is greater than b");
+}
+else
+{
+    Console.WriteLine("a is not greater than b");
+}
